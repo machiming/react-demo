3a4159fac43216ef670d3e2175e277899700b717
machiming <837944832@qq.com>
2018-11-10 13:55:27 +0800

    轮播

---
+++ src/component/common/BootSwiper.jsx
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import Swiper from 'swiper';
+import 'swiper/dist/css/swiper.min.css';
+
+class BootSwiper extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            bannerList:[
+            {"img":"http://img1.shikee.com/try/2016/06/25/14244120933639105658.jpg_220x220.jpg","url":""},
+            { "img":"http://img1.shikee.com/try/2016/06/23/15395220917905380014.jpg_220x220.jpg","url":""}
+         ]
+        }
+    }
+
+    componentDidMount() {
+        new Swiper(this.refs.a, {
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination'
+            },
+            autoplay: 2001,
+            paginationClickable: true
+        });
+    }
+
+
+    render() {
+        const swiperStyle = {
+            width: "100%",
+            height: "200px",
+            display: "flex"
+        };
+        return (
+            <div>
+                <div style={swiperStyle} className="swiper-container" ref="a">
+                    <div className="swiper-wrapper">
+                        {this.state.bannerList.map(v=>{
+                            return <div className="swiper-slide"><a href={v.url}><img src={v.img}/></a></div>
+                        })}
+
+                    </div>
+                    <div className="swiper-pagination"></div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default BootSwiper;
